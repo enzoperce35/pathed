5fa50e43c97ff10e26af6ef9f553c954ad934933
enzoperce35 <enzoperce35@gmail.com>
2022-03-14 04:52:36 +0800

    built seeding methods

---
+++ db/seedings.rb
@@ -0,0 +1,28 @@
+require 'csv'
+
+def languages
+  Language.create(name: 'Javascript')
+  Language.create(name: 'Html')
+  Language.create(name: 'Ruby')
+  Language.create(name: 'Rails')
+end
+
+def pages
+  file = File.new('pages.csv', 'r')
+
+  csv = CSV.new(file)
+  
+  csv.each do |row|
+    page_informations = {
+      content: row[1],
+      name: row[2],
+      link: row[3]
+    }
+
+    lang = Language.find_by(name: row[0])
+    
+    page = Page.create(page_informations)
+
+    lang.pages << page
+  end
+end
