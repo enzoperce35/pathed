48eed43cfd9b9ab43dfebfe14be2cc21c680acd6
enzoperce35 <enzoperce35@gmail.com>
2022-03-21 15:40:43 +0800

    modified select2 query for possible production solution

--- app/javascript/packs/select2.js
+++ app/javascript/packs/select2.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
     window.open(e.params.data.id);
     window.location.search += ('&selected=' + e.params.data.text);
   });
-});
 
-$(document).on('select2:open', () => {
-  console.log('select:open working');
-  document.querySelector('.select2-search__field').focus();
+  $(document).on('select2:open', () => {
+    console.log('select:open working');
+    document.querySelector('.select2-search__field').focus();
+  });
 });
