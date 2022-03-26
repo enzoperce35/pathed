ea9d11e75ed9336ed393948f1ba1d11a315b26f1
enzoperce35 <enzoperce35@gmail.com>
2022-03-27 07:30:35 +0800

    production solution

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
   def no_language_set
     all_lang = Language.all.pluck(:name)
     
-    current_language.nil? || !all_lang.include?(current_language)
+    session[:current_language].nil? || !all_lang.include?(current_language)
   end
 end
